4ccc700ffdd72009b6c060b25b9047b2ef14bb03
LeeGaKi <1004102689@qq.com>
2022-08-10 15:14:45 +0800

    hot-fix commit

--- demo2/src/main/java/icu/lijiaqi/demo2/controller/BookController.java
+++ demo2/src/main/java/icu/lijiaqi/demo2/controller/BookController.java
@@ -28,7 +28,7 @@ public class BookController {
     @PostMapping("/")
     @ApiOperation("2书籍")
     public boolean addBook(Books books){
-        System.out.println(123);
+        System.out.println(123345);
         return booksService.save(books);
     }
     @PutMapping("/")
